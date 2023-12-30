6121bbddd8d737f333c2336146041ea180880353
Mridul <sharmamridul045@gmail.com>
2023-12-30 12:36:49 +0000

    created api handler files

--- src/index.js
+++ src/index.js
@@ -1,22 +1,20 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+dotenv.config({ path: "./env" });
 
-dotenv.config({path: './env'});
-connectDB();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error:", error);
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongodb connection failed !!!", err);
+  });
 
 
 
@@ -35,6 +33,8 @@ connectDB();
 
 
 
+
+  
 /*import express from 'express';
 const app = express();
 (async()=>{
